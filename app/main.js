2211daa86a61e292c44b7d63cfebb711c34c4271
Andrew Haith <andyhaith@gmail.com>
2015-04-22 09:41:58 +0100

    Tests successfully running via grunt task

--- app/main.js
+++ app/main.js
@@ -1,9 +1,11 @@
-define(['backbone', 'layoutmanager', 'app/router/router'],
+define(['backbone', 'underscore', 'layoutmanager', 'app/router/router'],
 	function(Backbone, Layout, Router){
 
 		var App = {
 			onDeviceReady: function() {
+				//callback for tests
 				console.log("Device ready");
+
 				//configure Layoutmanager to manage views by default
 				Backbone.Layout.configure({ manage:true });
 
@@ -15,14 +17,8 @@ define(['backbone', 'layoutmanager', 'app/router/router'],
 				console.log("Started the app");
 			},
 
-			initialize: function() {
-				if (typeof(device) !== 'undefined') {
-					//wait for device ready event
-					document.addEventListener('deviceready', onDeviceReady, false);
-				} else {
-					//immediately ready in desktop browser
-					this.onDeviceReady();
-				}
+			initialize: function(cb) {
+				document.addEventListener('deviceready', _.bind(this.onDeviceReady, this), false);
 			}
 		};
 
